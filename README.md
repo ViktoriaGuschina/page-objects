72390e266b427a68708dc9496831d7b7c7a7899e
vladVashurin <vlad.vashurin@bk.ru>
2025-01-14 20:54:04 +0300

    tests

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Java CI with Gradle](https://github.com/ViktoriaGuschina/page-objects/actions/workflows/gradle.yml/badge.svg)](https://github.com/ViktoriaGuschina/page-objects/actions/workflows/gradle.yml)
--- src/test/java/ru/netology/web/page/LoginPage.java
+++ src/test/java/ru/netology/web/page/LoginPage.java
@@ -5,15 +5,15 @@ import ru.netology.web.data.DataHelper;
 
 import static com.codeborne.selenide.Selenide.$;
 
-    public class LoginPage {
-        private SelenideElement loginField = $("[data-test-id=login] input");
-        private SelenideElement passwordField = $("[data-test-id=password] input");
-        private SelenideElement loginButton = $("[data-test-id=action-login]");
+public class LoginPage {
+    private final SelenideElement loginField = $("[data-test-id=login] input");
+    private final SelenideElement passwordField = $("[data-test-id=password] input");
+    private final SelenideElement loginButton = $("[data-test-id=action-login]");
 
-        public VerificationPage validLogin(DataHelper.AuthInfo info) {
-            loginField.setValue(info.getLogin());
-            passwordField.setValue(info.getPassword());
-            loginButton.click();
-            return new VerificationPage();
-        }
+    public VerificationPage validLogin(DataHelper.AuthInfo info) {
+        loginField.setValue(info.getLogin());
+        passwordField.setValue(info.getPassword());
+        loginButton.click();
+        return new VerificationPage();
     }
+}
